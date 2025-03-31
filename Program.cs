329fdbeade71a0767f041d87c5e53788fc53e678
szakferi <szakfer97@gmail.com>
2025-03-31 16:52:40 +0300

    Fixed error bug

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using MoneyTracker.Services;
 using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<MoneyTracker.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
